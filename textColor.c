bc12bba2e771960206d18f7ee424caf544fa7350
prottoy-bhattacharyya <83195575+prottoy-bhattacharyya@users.noreply.github.com>
2024-04-21 15:56:27 +0600

    Update textColor.c

--- textColor.c
+++ textColor.c
@@ -8,3 +8,17 @@ int main()
    getch();
    return 0;
 }
+/*
+Color attributes are specified by TWO hex digits -- the first
+corresponds to the background; the second the foreground.  Each digit
+can be any of the following values:
+
+    0 = Black       8 = Gray
+    1 = Blue        9 = Light Blue
+    2 = Green       A = Light Green
+    3 = Aqua        B = Light Aqua
+    4 = Red         C = Light Red
+    5 = Purple      D = Light Purple
+    6 = Yellow      E = Light Yellow
+    7 = White       F = Bright White
+*/
